7e07a9154ed0ee7d69a1d516caa11f10d6bae8da
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-21 23:02:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,12 +41,12 @@ end
 def reduce_to_all_true(source_array)
   i = 0
   while i < source_array.length do
-  if source_array[i] = false
-    return false
+  unless source_array[i] = true
+    return true
  end
  i += 1
 end
-return true
+return false
 end
 
 
