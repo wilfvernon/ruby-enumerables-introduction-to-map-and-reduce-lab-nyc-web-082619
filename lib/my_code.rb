415ee895f15cf8bfd26c2216d78562e2ba481600
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-21 23:04:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,5 +49,17 @@ end
 return true
 end
 
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+  if source_array[i]
+    return true
+ end
+ i += 1
+end
+return false
+end
+
+
 
 
